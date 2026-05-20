6a13dd73c9e1877a070409029c338b04d3e910b7
Abhay Pratap Singh <abhay.pratap_cs23@gla.ac.in>
2026-05-20 22:51:26 +0530

    Refactor Slider component to use skills array
    Refactored Slider component to use a skills array for dynamic rendering of skill items.

--- PORTFOLIO/components/Slider.jsx
+++ PORTFOLIO/components/Slider.jsx
@@ -1,30 +1,72 @@
+function Slider() {
 
-function Slider(){
+    const skills = [
+        "Python",
+        "Java",
+        "HTML",
+        "CSS",
+        "Tailwind",
+        "React",
+        "JavaScript",
+        "SQL",
+        "Node",
+        "Express",
+    ]
+
     return (
-        <>
-            <div className="mt-40 mb-20 flex gap-10 p-3 m-20">
-                <div className="slider_lang motion-translate-x-in-[490%] motion-duration-9000 ">
-                    <h1>Python</h1>
+        <div className="overflow-hidden w-full py-10 my-40">
+
+            <div className="flex animate-scroll">
+
+                <div className="flex   gap-8 pr-8">
+                    {skills.map((item, index) => (
+                        <div key={index} className="slider_lang">
+                            <h1>{item}</h1>
+                        </div>
+                    ))}
                 </div>
 
-                <div className="slider_lang motion-translate-x-in-[340%] motion-duration-9000  bg-red-500 text-black">
-                    <h1>Java</h1>
+                <div className="flex  gap-8 pr-8">
+                    {skills.map((item, index) => (
+                        <div key={index} className="slider_lang">
+                            <h1>{item}</h1>
+                        </div>
+                    ))}
                 </div>
 
-                <div className="slider_lang motion-translate-x-in-[190%] motion-duration-9000  ">
-                    <h1>REACT</h1>
-                </div>
-
-                <div className="slider_lang motion-translate-x-in-[40%] motion-duration-9000  bg-red-500  text-black">
-                    <h1>JavaScript</h1>
-                </div>
-
-                <div className="slider_lang motion-translate-x-in-[10%] motion-duration-9000  w-1/6   ">
-                    <h1>SQL</h1>
-                </div>
             </div>
-        </>
+
+        </div>
     )
 }
 
 export default Slider
+// function Slider(){
+//     return (
+//         <>
+//             <div className="mt-40 mb-20 flex gap-10 p-3 m-20">
+//                 <div className="slider_lang motion-translate-x-in-[490%] motion-duration-9000 ">
+//                     <h1>Python</h1>
+//                 </div>
+
+//                 <div className="slider_lang motion-translate-x-in-[340%] motion-duration-9000  bg-red-500 text-black">
+//                     <h1>Java</h1>
+//                 </div>
+
+//                 <div className="slider_lang motion-translate-x-in-[190%] motion-duration-9000  ">
+//                     <h1>REACT</h1>
+//                 </div>
+
+//                 <div className="slider_lang motion-translate-x-in-[40%] motion-duration-9000  bg-red-500  text-black">
+//                     <h1>JavaScript</h1>
+//                 </div>
+
+//                 <div className="slider_lang motion-translate-x-in-[10%] motion-duration-9000  w-1/6   ">
+//                     <h1>SQL</h1>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default Slider
